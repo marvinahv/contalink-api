6063e683d3552afea8aec7ddd81643f66365b217
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2025-01-21 16:27:36 -0600

    add get /invoices route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # invoices
+  get "invoices", to: "invoices#index"
 end
