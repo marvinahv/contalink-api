7ecd0e4f94ac84f6903c62cbfa9a23f4c33a8fc3
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2025-01-21 17:23:12 -0600

    configure dev adapter for background jobs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Use async for development background jobs
+  config.active_job.queue_adapter = :async
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
